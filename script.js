125d92c3db126181ab65911302b8ed9b63c83d44
ooo <leeye.ooo3@gmail.com>
2022-11-06 18:36:40 +0900

    used to local storage & event arg

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form > input");
-
 const loginButton = document.querySelector("#login-form > button");
 
-function nameValidation(){
+const greeting = document.querySelector("#greeting");
 
-    const loginName = loginInput.value
-    if (loginName === ""){
-        alert("plz write ur name")
-    }else if(loginName.length > 15){
-        alert("name is too long")
-    }
+//define key
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
+//local storage
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+
+if(savedUserName == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",formSubmit);
+}else{
+    paintGreetings(savedUserName)
 }
 
+function paintGreetings(username_){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username_}`
+}
 
-loginButton.addEventListener("click",nameValidation>)
+function formSubmit(e){
+    e.preventDefault();
+    const username_input = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username_input);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username_input);
+}   
+
+
